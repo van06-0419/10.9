eb40e17daaa0246ae21868bd3daaef3dd1288c1a
Wangzy0606 <344182022@qq.com>
2025-09-10 23:18:28 +0700

    second times

---
+++ lib.cpp
@@ -0,0 +1,7 @@
+#include "lib.h"
+#include <string>
+#include "version.h"
+
+std::string getVersion() {
+    return VERSION_STRING;
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "lib.h"
+
+int main() {
+    std::cout <<"Hello World! Version" << getVersion()<< std::endl;
+    return 0;
+}
